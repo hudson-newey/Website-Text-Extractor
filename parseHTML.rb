2e37c9e5ee09f05755b3bfcddaf1d0a39b683fb3
Grathium-Industries <GrathiumIndustries@gmail.com>
2021-06-13 21:24:13 +1000

    parser now removes artifacts through a rule list (rather than hard coded)

--- parseHTML.rb
+++ parseHTML.rb
@@ -10,22 +10,15 @@ def removeArtifacts(text)
 end
 
 def findText(websiteHTML)
-    # remove all the script tags from the html
-    @matchRegex = /<script.+?>.+?</
-    @foundScriptTags = websiteHTML.to_enum(:scan, @matchRegex).map { Regexp.last_match }
-
-    @foundScriptTags.each do |item|
-        item = convertToString([item])
-        websiteHTML = websiteHTML.gsub(item, "")
-    end
-
-    # remove all the style tags
-    @matchRegex = /<style.+?>.+?</
-    @foundStyleTags = websiteHTML.to_enum(:scan, @matchRegex).map { Regexp.last_match }
-
-    @foundStyleTags.each do |item|
-        item = convertToString([item])
-        websiteHTML = websiteHTML.gsub(item, "")
+    @artifactRules = [/<script.+?>.+?</, /<script.+?>/, /<style.+?>.+?</]
+    # remove all artifacts from the html source
+    @artifactRules.each do |rule|
+        @foundScriptTags = websiteHTML.to_enum(:scan, rule).map { Regexp.last_match }
+        
+        @foundScriptTags.each do |item|
+            item = convertToString([item])
+            websiteHTML = websiteHTML.gsub(item, "")
+        end
     end
 
     # find all inner text (this text will contain artifacts "<" & ">")
